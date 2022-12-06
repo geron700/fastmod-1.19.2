9f3bc19484161171297486518910c8a74f6af0ad
geron700 <geron700@gmail.com>
2022-12-06 15:39:49 +0100

    +dalibor_block

--- src/main/java/net/geron/fastmod/fastmod.java
+++ src/main/java/net/geron/fastmod/fastmod.java
@@ -1,6 +1,7 @@
 package net.geron.fastmod;
 
 import com.mojang.logging.LogUtils;
+import net.geron.fastmod.block.ModBlocks;
 import net.geron.fastmod.item.ModItems;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -22,6 +23,7 @@ public class fastmod {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
